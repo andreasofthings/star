cf322ae9307afbad0f7485f04fcdb00952c64291
Andreas Neumeier <andreas@neumeier.org>
2015-08-04 21:32:58 +0200

    setup.py & requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 """Setup file for easy installation"""
 from os.path import join, dirname
-from setuptools import setup, find_packages
+from setuptools import setup
 
 version = __import__('star').__version__
 
 LONG_DESCRIPTION = """
 """
 
+
 def long_description():
     """
     """
     try:
-        return open(join(dirname(__file__), 'README.md')).read()
+        return open(join(dirname(__file__), 'README.rst')).read()
     except IOError:
         return LONG_DESCRIPTION
 
@@ -24,17 +25,20 @@ setup(name='django-ilike',
       description='Django liking and starring simple.',
       license='BSD',
       keywords='django, like, star, track, application',
-      url='https://github.com/andreasneumeier/star',
-      download_url='https://github.com/andreasneumeier/star',
-      packages=['star', 'star.templatetags',],
+      url='https://github.com/aneumeier/star',
+      download_url='https://github.com/aneumeier/star',
+      packages=['star', 'star.templatetags', ],
       package_data={'star': ['locale/*/LC_MESSAGES/*']},
       long_description=long_description(),
-      install_requires=['django>=1.4.1', ],
       classifiers=['Framework :: Django',
                    'Development Status :: 4 - Beta',
                    'Topic :: Internet',
                    'License :: OSI Approved :: BSD License',
                    'Intended Audience :: Developers',
                    'Environment :: Web Environment',
-                   'Programming Language :: Python :: 2.7'])
-
+                   'Programming Language :: Python :: 2.7'],
+      install_requires=[
+        'django>=1.4.1',
+        'pillow',
+      ],
+      )
