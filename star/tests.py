17ae89eaba24e04d0616a41ba17ed50fa378749a
Andreas Neumeier <andreas@neumeier.org>
2015-08-04 21:51:09 +0200

    One shot.

--- star/tests.py
+++ star/tests.py
@@ -6,14 +6,24 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from models import Star
+from .models import Star
 
 class StarTest(TestCase):
     def setUp(self):
         self.star = Star()
+        self.client = Client()
+        """Test Client."""
 
-    def test_basic_addition(self):
+    def test_badge(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests to request a badge.
         """
-        self.assertEqual(1 + 1, 2)
+        self.result = self.client.get(reverse('star:options'))
+        self.assertEqual(result.status_code, 302)
+
+    def test_demo(self):
+        """
+        Tests to request the demo page.
+        """
+        self.result = self.client.get(reverse('star:demo'))
+        self.assertEqual(result.status_code, 302)
--- star/urls.py
+++ star/urls.py
@@ -1,27 +1,47 @@
-from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import url, patterns
 
 from django.views.generic import TemplateView
-import views
 
-urlpatterns = patterns('star.views',
-  url(r'^$', 'count', name='tracking_star'),
-  url(r'^/score$', 'count_score', name='count_score'),
-  url(r'^/vote/up$', 'count_up', name='count_up'),
-  url(r'^/vote/down$', 'count_down', name='count_down'),
-  url(r'^/static/count.js$', TemplateView.as_view(template_name='count.js'), name='count_js'),
-  url(r'^/static/count.css$', TemplateView.as_view(template_name='count.css'), name='count_css'),
+urlpatterns = patterns(
+    'star.views',
+    url(r'^$', 'count', name='tracking_star'),
+    url(r'^/score$', 'count_score', name='count_score'),
+    url(r'^/vote/up$', 'count_up', name='count_up'),
+    url(r'^/vote/down$', 'count_down', name='count_down'),
+    url(
+        r'^/static/count.js$',
+        TemplateView.as_view(template_name='count.js'),
+        name='count_js'
+    ),
+    url(
+        r'^/static/count.css$',
+        TemplateView.as_view(template_name='count.css'),
+        name='count_css'
+    ),
 )
 
 
-urlpatterns += patterns('star.views',
-  # include this
-  url(r'^/star.js$', TemplateView.as_view(template_name="star/star.js"), name="star_js"),
-  # receives data 
-  url(r'^/star$', 'star_callback', name='star_callback'),
-  # style
-  url(r'^/static/star.css$', TemplateView.as_view(template_name="star/star.css"), name='star_css'),
-  
-  # demo.html
-  url(r'^/demo.html$', TemplateView.as_view(template_name="star/demo.html"), name='star_demo'),
+urlpatterns += patterns(
+    'star.views',
+    # include this
+    url(
+        r'^/star.js$',
+        TemplateView.as_view(template_name="star/star.js"),
+        name="js"
+    ),
+    # receives data
+    url(r'^/star$', 'star_callback', name='star_callback'),
+    # style
+    url(
+        r'^/static/star.css$',
+        TemplateView.as_view(template_name="star/star.css"),
+        name='css'
+    ),
+
+    # demo.html
+    url(
+        r'^/demo.html$',
+        TemplateView.as_view(template_name="star/demo.html"),
+        name='demo'
+    ),
 )
